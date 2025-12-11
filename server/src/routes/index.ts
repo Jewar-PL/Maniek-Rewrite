60160b9a04870fea8b21254004c839077606deb4
Jewar-PL <a.batorski.jewar@gmail.com>
2025-12-11 12:12:11 +0100

    Moved streaming logic into a controller, added a video router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
+import videoRouter from "./video.routes";
+
 const router = Router();
 
 // Example route
 router.get("/", (req, res) => {
     res.status(200).json({ message: "I love you", sender: "Von Lycaon", recipient: "QT" });
 });
+
+router.use("/video", videoRouter);
 
 export default router;
