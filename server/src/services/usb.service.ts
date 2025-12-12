5997acfbe5679d7d8c055e7d9d476bf089f8a5b2
Jewar-PL <a.batorski.jewar@gmail.com>
2025-12-12 11:03:49 +0100

    Added folder structure checking, temporary import of service to index

--- server/src/services/usb.service.ts
+++ server/src/services/usb.service.ts
@@ -1,12 +1,15 @@
 import { EventEmitter } from "events";
 import fs from "fs";
+import fsPromises from "fs/promises";
 import path from "path";
 
 import { usb, Device } from "usb";
-import drivelist from "drivelist";
+import * as drivelist from "drivelist";
 
 
 // TODO: Simplify the interfaces???
+
+const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 interface USBServiceEvents {
     "usbAttached": (dev: Device) => void;
@@ -32,15 +35,70 @@ class USBService extends EventEmitter {
     }
 
     private async onAttach(dev: Device) {
-        // TODO: Do something here
-        console.log("USB attached:\n", JSON.stringify(dev.deviceDescriptor, null, 2));
+        // The handle waits a short amount of time to make sure the drive mounts properly
+        // TODO: Take care of this in a better way
+        await wait(1500);
+
+        // 1. Get eligible mountpoint
+        // 2. Check if content in project_root/server/videos is different than mountpoint_root
+        // 3. Swap out videos if they are different
+        // 4. Emit event
+
         this.emit("usbAttached", dev);
     }
 
     private async onDetach(dev: Device) {
         // TODO: Do something here, or remove?
-        console.log("USB detached:\n", JSON.stringify(dev.deviceDescriptor, null, 2));
         this.emit("usbDetached", dev);
+    }
+
+    // TODO: Pass a drive as an argument instead of checking all of them here?
+    // TODO: Remove logs
+    /**
+     * Gets the first mountpoint that belongs to a removable drive and has proper folder structure
+     * @returns First eligible mountpoint if one was found, otherwise null
+     */
+    private async getEligibleMountpoint() {
+        const drives = await drivelist.list();
+
+        const usbDrives = drives.filter(drive => 
+            drive.isRemovable &&
+            drive.mountpoints &&
+            drive.mountpoints.length !== 0
+        );
+
+        for (const drive of usbDrives) {
+            console.log(`Checking drive: ${drive.description}`);
+
+            for (const mp of drive.mountpoints) {
+                console.log(`Checking mountpoint: ${mp.path}`);
+
+                const hasProperStructure = await this.hasProperFolderStructure(mp);
+                
+                if (hasProperStructure) return mp;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks if the specified mountpoint has both `priority` and `regular` folders at root
+     * @param dir Mountpoint in which the function checks the constraint
+     * @returns `true` if it has the proper structure, `false` otherwise
+     */
+    private async hasProperFolderStructure(mp: drivelist.Mountpoint) {
+        // Check if the directory has priority/ and regular/ subdirectories
+        const pathPriority = path.join(mp.path, "priority");
+        const pathRegular = path.join(mp.path, "regular");
+
+        // TODO: Check if it throws
+        const [priority, regular] = await Promise.all([
+            fsPromises.stat(pathPriority).catch(() => null),
+            fsPromises.stat(pathRegular).catch(() => null),
+        ]);
+
+        return Boolean((priority && priority.isDirectory()) && (regular && regular.isDirectory()));
     }
 }
 
