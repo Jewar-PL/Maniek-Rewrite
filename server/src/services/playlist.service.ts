43ce9ae0256206ca91c0cd0fbaf493977377836b
Jewar-PL <a.batorski.jewar@gmail.com>
2025-12-14 18:53:28 +0100

    Moved arranging playlist into a separate method

--- server/src/services/playlist.service.ts
+++ server/src/services/playlist.service.ts
@@ -8,28 +8,36 @@ class PlaylistService {
 
     public constructor() {
         usbService.on("usbAttached", async(ev) => await this.onUSBAttached(ev.filesChanged));
+    
+        // Get the playlist for the first time
+        this.refreshPlaylist();
     }
 
     private async onUSBAttached(filesChanged: boolean) {
-        if (filesChanged) {
-            await this.refreshPlaylist();
-        }
+        if (filesChanged) await this.refreshPlaylist();
     }
 
     private async refreshPlaylist() {
+        console.log(`Refreshing playlist...`);
+
         const videosPath = path.join(__dirname, "..", "..", "videos");
         const priorityPath = path.join(videosPath, "priority");
         const regularPath = path.join(videosPath, "regular");
 
         try {
-            // TODO: Actual playlist algorithm
-            const priorityFiles = await fsPromises.readdir(priorityPath);
-            const regularFiles = await fsPromises.readdir(regularPath);
-            this._playlist = [...priorityFiles, ...regularFiles];
+            const priorityFiles = (await fsPromises.readdir(priorityPath)).map(file => `priority/${file}`);
+            const regularFiles = (await fsPromises.readdir(regularPath)).map(file => `regular/${file}`);
+            
+            this._playlist = this.arrangePlaylist(priorityFiles, regularFiles);
         } catch (err) {
             console.error(`Error refreshing playlist: ${err}`);
             this._playlist = [];
         }
+    }
+
+    private arrangePlaylist(priority: string[], regular: string[]): string[] {
+        // TODO: Actual algorithm
+        return [...priority, ...regular];
     }
 
     public get playlist(): string[] {
